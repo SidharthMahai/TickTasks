4009ec7e71768a7aaecaba73523b6d951d62b5b0
SidharthMahai <sidharth.mahai.work@gmail.com>
2022-09-25 11:14:04 +0530

    Add: blank navbar and tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavbarComponent,
+      TasksComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
