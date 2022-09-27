bf355c53b8fb8bb0a77e79151eb2808de3df2efe
SidharthMahai <sidharth.mahai.work@gmail.com>
2022-09-27 12:31:50 +0530

    Changes in tasks page, service and model

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,13 +1,7 @@
-import { Time } from "@angular/common";
-
-class Task {
+export class Task {
+    id: string;
     Task_Name: string;
-    Task_Description: string;
-    Task_Priority: number;
     Task_Created_DateTime: Date;
     Task_Completion_DateTime: Date;
-    Task_Started: boolean;
     Task_Completed: boolean;
-    Task_Countdown_Wanted: boolean;
-    Task_Countdown_Time: Time;
 }
