3acc2fca023a50a689c6f141f08521f2a73eb631
SidharthMahai <sidharth.mahai.work@gmail.com>
2022-09-27 12:29:44 +0530

    Changes in tasks model, service and tasks page

--- src/app/tasks-service.service.ts
+++ src/app/tasks-service.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Task } from 'src/models/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksServiceService {
-public user_name: string;
-constructor() { }
+  public user_name: string;
+  public tasks: Array<Task> = [];
+  constructor() {}
 
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  generateRandomId(length: number): string {
+    var result = '';
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  deleteTask(task: Task) {
+    let index = this.tasks.findIndex(d => d.id === task.id);
+    this.tasks.splice(index, 1);
+  }
+  
 }
